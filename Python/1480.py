5f74c3bf5e14cff13d080865613801c4a723d4ab
SushiCat <55708782+Sushi6006@users.noreply.github.com>
2022-07-12 18:43:40 +0800

    Update 1480.py

--- Python/1480.py
+++ Python/1480.py
@@ -1,10 +1,5 @@
-# 06/17/2020 19:32
-
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        tot = 0
-        for i in range(len(nums)):
-            tot += nums[i]
-            nums[i] = tot
-        
+        for i in range(1, len(nums)):
+            nums[i] += nums[i - 1]
         return nums
